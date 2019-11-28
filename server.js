090d8e76979ea6af7fe70dc373e253e3f25c61d9
Caio Senna <caiosenna20@outlook.com>
2019-11-28 00:27:31 -0300

    Meu primeiro commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+let port = 3000
+
+app.listen(port,() => {
+    console.log('Servidor rodando na porta ' +  port)
+})
+
+app.get('/',(req,res) => {
+    res.send("Minha primeira rota")
+
+})
+
+
+app.get('/teste',(req,res) => {
+    res.send("Meu primeiro teste")
+
+})
+
+
